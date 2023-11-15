de6485b2d6bfdae38368d621dd1462669a82e7b6
Malleswaramma <malleswarammaulli@gmail.com>
2023-11-15 10:56:03 +0530

    intial commit

---
+++ src/main/java/com/app/Welcome.java
@@ -0,0 +1,13 @@
+package com.app;
+
+public class Welcome {
+	public static void main(String[] args) {
+		
+		
+		for(int i=0;i<100;i++) {
+			System.out.println(" number is "+ i);
+		}
+		System.out.println("anil");
+	}
+
+}
